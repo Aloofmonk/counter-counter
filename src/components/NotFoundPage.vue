876dfadb338c59eba58be35284bebfddd01cdc90
Benjamin Nomba <bnomba0704@gmail.com>
2023-03-20 10:12:47 -0700

    stlyefifth

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -1,4 +1,24 @@
 <template>        
-		<h1>404 not found</h1>
-		<h2>it seems you're in the wrong page</h2>
+	<div class="text-cont">
+		<div class="text">
+			<h1>404 not found</h1>
+			<h2>Oops! it seems you're in the wrong page, navigate your way back!</h2>
+		</div>
+	</div>
 </template>
+
+<style scoped>
+	.text{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 50vh;	
+	}
+	h1{
+		font-size: 80px;
+	}
+	h2{
+		color: rgb(186, 182, 182);
+	}
+</style>
