b68eeba34515be8d26cb20630f934f2964096ec3
Benjamin Nomba <bnomba0704@gmail.com>
2023-03-20 11:46:59 -0700

    stylesix

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="about">
 		<div class="acontainer">
-
 			<p>Welcome to The Counter. Its purpose is to help count increments and decrements of 1s while at the same time offering a set value, which allows the user input a number and also a reset feature to unset the counter back to 0. Use the navigation link provided to direct you to the Counter, have fun but not too much fun!</p>
 		</div>
 
@@ -16,6 +15,10 @@
 }
 .acontainer{
 	width: 70%;
+	border-left: 5px solid grey;
+	border-right: 5px solid grey;
+	padding-left: 10px;
+	padding-right: 5px;
 }
 p{
 	text-align: left;
